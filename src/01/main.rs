22002c5418dcd1b1c2e7f3fc850ae9e7feebc2fc
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2022-12-01 06:32:34 +0100

    01: test and fix parsing

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,11 +1,12 @@
 use std::fs;
 
 fn main() {
-    let file_path = "/Users/timgohlisch/Documents/Advent_of_Code/_2022/src/01/input.txt";
+    let file_path = "src/01/input.txt";
     let contents = fs::read_to_string(file_path)
         .expect("Couldn't read input.txt! :(");
 
     let mut callories_vector = get_calories_vector(contents);
+    println!("Highest calories: {}", callories_vector.len());
     callories_vector.sort();
     println!("Highest calories: {}", callories_vector[callories_vector.len()-1]);
     println!("Top 3 calories: {}", callories_vector[callories_vector.len()-1]+callories_vector[callories_vector.len()-2]+callories_vector[callories_vector.len()-3]);
@@ -24,15 +25,21 @@ fn get_calories_vector (mut input: String) -> Vec<i32> {
             current_callories = 0;
         }
     }
+    callories_vector.push(current_callories);
 
     callories_vector
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::get_calories_vector;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn sums_up_single_meals() {
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+
+        let calories_vector = get_calories_vector(input.to_string());
+
+        assert_eq!(calories_vector.len(), 5);
     }
 }
