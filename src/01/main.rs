70f5357306b3b5a62965e9569667d4934916cb94
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2022-12-01 06:19:35 +0100

    day 01

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,5 +1,31 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "/Users/timgohlisch/Documents/Advent_of_Code/_2022/src/01/input.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Couldn't read input.txt! :(");
+
+    let mut callories_vector = get_calories_vector(contents);
+    callories_vector.sort();
+    println!("Highest calories: {}", callories_vector[callories_vector.len()-1]);
+    println!("Top 3 calories: {}", callories_vector[callories_vector.len()-1]+callories_vector[callories_vector.len()-2]+callories_vector[callories_vector.len()-3]);
+}
+
+fn get_calories_vector (mut input: String) -> Vec<i32> {
+    let mut callories_vector: Vec<i32> = vec![];
+    let mut current_callories = 0;
+
+    for line in input.lines() {
+        let calories = line.parse::<i32>();
+        if calories.is_ok() {
+            current_callories = current_callories + calories.unwrap();
+        } else {
+            callories_vector.push(current_callories);
+            current_callories = 0;
+        }
+    }
+
+    callories_vector
 }
 
 #[cfg(test)]
